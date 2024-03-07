fd7d235ceaa59e3d60a593f71568f4adec095c33
hexoN <aliakbaripour00@gmail.com>
2024-03-07 12:39:38 +0330

    chore(interactive_page): The todos are now stored in an array and are being shown on the webpage by using loops. The added todos can be deleted too. You can also add dates for them.

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
 let todoList = [];
-const todoName = document.querySelector('.todo-name');
 
-function addTodo() {
-  todoList.push(todoName.value);  
-  console.log(todoList);
-  
-  todoName.value = '';
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML;
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+    const HTML = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button class="delete-button"
+    onclick="
+    todoList.splice(${i}, 1);
+    renderTodoList();">
+    Delete
+    </button>
+    `
+    todoListHTML += HTML
+  }
+
+  document.querySelector('.todo-list').innerHTML = todoListHTML;
 }
 
-function sendOnEnter() {
-  if (event.key === 'Enter') {
-    addTodo();
-  }
+// function addTodo() {
+//   const todoName = document.querySelector('.todo-name');
+//   const name = todoName.value;
+  
+//   const todoDate = document.querySelector('.todo-date');
+//   const dueDate = todoDate.value;
+
+//   todoList.push(
+//     todoDate,
+//     dueDate,
+//   );  
+  
+//   todoName.value = '';
+
+//   renderTodoList();
+// }
+
+ function addTodo() {
+     const todoName = document.querySelector('.todo-name').value;
+     const todoDate = document.querySelector('.todo-date').value;
+     
+     const newTodo = {
+       name: todoName,
+       dueDate: todoDate
+     };
+     
+     todoList.push(newTodo);
+     
+     renderTodoList();
+     
+     document.querySelector('.todo-name').value = '';
+     document.querySelector('.todo-date').value = '';
+   }
+
+  function sendOnEnter() {
+    if (event.key === 'Enter') {
+      addTodo();
+    }
 }
