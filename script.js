909de7bc198da7316b65d73fb50efd4f2a39c00d
hexoN <aliakbaripour00@gmail.com>
2024-05-09 09:44:52 +0330

    Cleared the code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
-let checkedItems = JSON.parse(localStorage.getItem('checked')) || [];
-const todoInput = document.querySelector('.todo-name');
-const todoSend = document.querySelector('.add-todo');
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+let checkedItems = JSON.parse(localStorage.getItem("checked")) || [];
+const todoInput = document.querySelector(".todo-name");
+const todoSend = document.querySelector(".add-todo");
 
 renderTodoList();
 function renderTodoList() {
-  let todoListHTML = '';
-  const todoListContainerHTML = document.querySelector('.todo-list');
+  let todoListHTML = "";
+  const todoListContainerHTML = document.querySelector(".todo-list");
 
   todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
@@ -23,40 +23,40 @@ function renderTodoList() {
           <i class='bx bxs-trash'></i>
         </button>
       </li>
-    `
-    todoListHTML += HTML
-  })
+    `;
+    todoListHTML += HTML;
+  });
   todoListContainerHTML.innerHTML = todoListHTML;
   seperateOnRender();
 
-  document.querySelectorAll('.todo-checkbox').forEach((checkbox, index) => {
-    checkbox.addEventListener('click', (event) => {
-      const todoItem = document.querySelectorAll('.todo-item')[index];
-      todoItem.classList.toggle('todo-item-done', event.target.checked);
+  document.querySelectorAll(".todo-checkbox").forEach((checkbox, index) => {
+    checkbox.addEventListener("click", (event) => {
+      const todoItem = document.querySelectorAll(".todo-item")[index];
+      todoItem.classList.toggle("todo-item-done", event.target.checked);
       checked = true;
-      localStorage.setItem('checked', JSON.stringify(checked));
+      localStorage.setItem("checked", JSON.stringify(checked));
     });
   });
 }
 
 function seperateOnRender() {
-  const todoListContainerHTML = document.querySelector('.todo-list');
-  const seperator = document.querySelector('.seperator-off');
+  const todoListContainerHTML = document.querySelector(".todo-list");
+  const seperator = document.querySelector(".seperator-off");
 
   if (todoListContainerHTML.children.length > 0) {
-    seperator.classList.add('seperator');
+    seperator.classList.add("seperator");
   } else {
-    seperator.classList.remove('seperator');
+    seperator.classList.remove("seperator");
   }
 }
 
 function addTodo() {
-  const todoName = document.querySelector('.todo-name').value;
-  const todoDate = document.querySelector('.todo-date').value;
-  
-  if (todoName === '') {
-   alert('Please fill the inputs in order to add a new todo.');
-   return;
+  const todoName = document.querySelector(".todo-name").value;
+  const todoDate = document.querySelector(".todo-date").value;
+
+  if (todoName === "") {
+    alert("Please fill the inputs in order to add a new todo.");
+    return;
   }
 
   const newTodo = {
@@ -64,23 +64,27 @@ function addTodo() {
     dueDate: todoDate,
     checked: false,
   };
-  
+
   todoList.push(newTodo);
-  localStorage.setItem('todoList', JSON.stringify(todoList));
+  localStorage.setItem("todoList", JSON.stringify(todoList));
   renderTodoList();
-  document.querySelector('.todo-name').value = '';
-  document.querySelector('.todo-date').value = '';
+  document.querySelector(".todo-name").value = "";
+  document.querySelector(".todo-date").value = "";
 }
 
-todoSend.addEventListener('click', () => addTodo());
+todoSend.addEventListener("click", () => addTodo());
 
-todoInput.addEventListener('keydown', () => { if (event.key === 'Enter') { addTodo() }}) 
+todoInput.addEventListener("keydown", () => {
+  if (event.key === "Enter") {
+    addTodo();
+  }
+});
 
-const checkboxElement = document.querySelector('.todo-checkbox');
+const checkboxElement = document.querySelector(".todo-checkbox");
 
 function lineThroughTodo() {
-  const checkedBox = checkboxElement.checked
-  const todoItem = document.querySelector('.todo-item');
+  const checkedBox = checkboxElement.checked;
+  const todoItem = document.querySelector(".todo-item");
 
-  todoItem.classList.toggle('todo-item-done', checkedBox)
+  todoItem.classList.toggle("todo-item-done", checkedBox);
 }
